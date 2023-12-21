00f7ad5e22c38248fcf74f35342794587def586c
Rome Bell <romebell5@gmail.com>
2023-12-20 21:24:29 -0800

    feat: add about page

---
+++ src/app/about/page.js
@@ -0,0 +1,27 @@
+"use client"
+import { useState, useEffect } from "react";
+
+export default function About() {
+    // state is what the data is representing in realtime
+    const [data, setData] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+  
+    useEffect(() => {
+      fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/`)
+      .then((res) => res.json())
+      .then((data) => {
+        // data is an object
+        setData(data);
+        setLoading(false);
+      })
+    }, []);
+  
+    if (isLoading) return <p>Loading...</p>
+    if (!data) return <p>No data shown...</p>
+  
+    return (
+      <main>
+        <h1>This is the about page</h1>
+      </main>
+    )
+  }
